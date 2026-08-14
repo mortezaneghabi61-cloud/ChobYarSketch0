28ff388eb5131093c0a8038a6942eeb88c0d737a
mortezaneghabi61-cloud <mortezaneghabi61@gmail.com>
2026-08-15 00:03:55 +0330

    Update MainActivity.java

--- app/src/main/java/ir/chobyar/sketch/MainActivity.java
+++ app/src/main/java/ir/chobyar/sketch/MainActivity.java
@@ -22,7 +22,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+);
         drawingView = new DrawingView();
 
         Button freeButton = makeButton("✏ آزاد", () ->
