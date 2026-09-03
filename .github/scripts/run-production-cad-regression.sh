b704ae4b09c14e051a17c12e1deb261231b6f1a5
mortezaneghabi61-cloud <mortezaneghabi61@gmail.com>
2026-09-03 08:10:42 +0330

    K3.22: include Offset authority in production regression

--- .github/scripts/run-production-cad-regression.sh
+++ .github/scripts/run-production-cad-regression.sh
@@ -80,6 +80,7 @@ run_contract K318DisconnectAuthorityInstrumentationTest disconnect-authority 'OK
 run_contract K319LinearArrayAuthorityInstrumentationTest linear-array-authority 'OK (4 tests)'
 run_contract K320TrimAuthorityInstrumentationTest trim-authority 'OK (4 tests)'
 run_contract K321ExtendAuthorityInstrumentationTest extend-authority 'OK (4 tests)'
+run_contract K322OffsetAuthorityInstrumentationTest offset-authority 'OK (4 tests)'
 run_contract K36dMidpointAuthorityInstrumentationTest midpoint-authority 'OK (5 tests)'
 run_contract PinchZoomInstrumentationTest pinch 'OK (1 test)'
 run_contract SketchUndoRedoInstrumentationTest sketch-undo-redo 'OK (1 test)'
